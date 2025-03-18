d0082252afe581db825a4fb03691350d6eb180b2
Pumarfa <pumarfa@gmail.com>
2025-03-18 07:10:24 -0300

    Se Incorpora la función de modificar roles de un usuario
     Cambios a ser confirmados:
    	modificados:     backend/src/routes/admin_routes.py
    Se incorpora la función "update_user_roles" que recibe el ID de usuario
    al que se le modifica los roles. Recibe roles: una lista con los ID de
    roles.
    Modifica la asignación de roles en la tabla intermedia de UserRole
    asignando los roles que existen en la tabla Roles.
    Si en la lista de entrada recibe un rol_id que no está en la tabla de
    Roles, lo descarta.

--- backend/src/routes/admin_routes.py
+++ backend/src/routes/admin_routes.py
@@ -25,7 +25,7 @@ def get_users(db: Session = Depends(get_db), current_user: dict = Depends(get_cu
     users = db.query(User).all()
     return users
 
-@admin_router.get("{user_id}", response_model=UserOut, description="Obtener un usuario por ID")
+@admin_router.get("/{user_id}", response_model=UserOut, description="Obtener un usuario por ID")
 def get_user(user_id: str, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     """
     Obtener un usuario por ID (Sólo para Administradores).
@@ -44,10 +44,46 @@ def get_user(user_id: str, db: Session = Depends(get_db), current_user: dict = D
         )
     return user
 
-@admin_router.put("{user_id}", response_model=UserOut, description="Modificar los datos de un usuario")
-def update_user(user_id: str, user_in: UserOut, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
+@admin_router.put("/{user_id}", response_model=UserOut, description="Modificar los datos de un usuario")
+def update_user(user_id: str, user_in: UserUpdate, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     """
     Modificar los datos de un usuario por ID (Sólo para Administradores).
+    """
+    # Verificar si el usuario tiene el rol "admin"
+    if not has_user_role(current_user, ["admin"]):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="No tienes permisos para realizar esta acción",
+        )
+    
+    #print(f"Update_User Admin - Password: {user_in.password}") # Debug
+    
+    # Buscar el usuario en la base de datos
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario no encontrado",
+        )
+    
+    # Actualizar los datos del usuario
+    if user_in.email:
+        user.email = user_in.email # Actualizar el correo electrónico si se proporciona y no hay duplicados
+    if user_in.password:
+        validar_password(user_in.password)
+        user.hashed_password = get_password_hash(user_in.password)
+    
+    # Guardar los cambios
+    db.commit()
+    db.refresh(user)
+    
+    # Devolver el usuario actualizado
+    return user
+
+@admin_router.put("/{user_id}/roles", response_model=UserOut, description="Modificar los roles de un usuario")
+def update_user_roles(user_id: str, roles: List[int], db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
+    """
+    Modificar los roles de un usuario por ID (Sólo para Administradores).
     """
     # Verificar si el usuario tiene el rol "admin"
     if not has_user_role(current_user, ["admin"]):
@@ -63,13 +99,17 @@ def update_user(user_id: str, user_in: UserOut, db: Session = Depends(get_db), c
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Usuario no encontrado",
         )
-    # print(f"Update_User Admin - Password: {user_in.password}") # Debug
-    # Actualizar los datos del usuario
-    if user_in.email:
-        user.email = user_in.email # Actualizar el correo electrónico si se proporciona y no hay duplicados
-    if user_in.password:
-        validar_password(user_in.password)
-        user.password = get_password_hash(user_in.password)
+    
+    # Buscar los roles en la base de datos
+    roles_db = db.query(Role).filter(Role.id.in_(roles)).all()
+    if not roles_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Roles no encontrados",
+        )
+    
+    # Actualizar los roles del usuario
+    user.roles = roles_db
     
     # Guardar los cambios
     db.commit()
